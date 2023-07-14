ea196eb339e791d2671b541de02d932ec033bb4a
germax33 <138248155+germax33@users.noreply.github.com>
2023-07-14 14:31:50 -0500

    Minimarket, productos.py

---
+++ Minimarket, productos.py
@@ -0,0 +1,6 @@
+# TI-Senatidef listar_productos(productos):
+    for nombre, detalles in productos.items():
+        print("nombre:{nombre}")
+        print("Marca:{marca}")
+        print("Precio:{precio}")
+        print("")
